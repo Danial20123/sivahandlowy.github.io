ceaa45ebcae5a2084ab445b18e94c58cc878e7c0
Danial20123 <118391154+Danial20123@users.noreply.github.com>
2023-07-10 07:35:58 -0700

    Update scriptbar.js

--- scriptbar.js
+++ scriptbar.js
@@ -1,9 +1,11 @@
 function openNav() {
     document.getElementById("mySidebar").classList.add("open");
     document.getElementById("overlay").style.display = "block";
+    document.body.classList.add("sidebar-open"); // Add this line
 }
 
 function closeNav() {
     document.getElementById("mySidebar").classList.remove("open");
     document.getElementById("overlay").style.display = "none";
+    document.body.classList.remove("sidebar-open"); // Add this line
 }
