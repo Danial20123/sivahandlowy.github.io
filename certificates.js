bbae03d2826768d599a1070e4ef69abc43ccf574
Danial20123 <118391154+Danial20123@users.noreply.github.com>
2023-07-16 04:19:40 -0700

    Update certificates.js

--- certificates.js
+++ certificates.js
@@ -14,19 +14,19 @@ function redirectPage() {
     else if (input === "SHS/QMS/PK-23001 13485") {
         window.location.href = "certificates/nn13485.pdf";
       } 
-      else if (input === "SHS/QMS/PL/0001 ce") {
+      else if (input === "SHS/QMS/PL/0001 CE") {
         window.location.href = "certificates/Document_2023-02-13_122512.pdf";
       } 
 
-      else if (input === "SHS/QMS/PK-23001 ce") {
+      else if (input === "SHS/QMS/PK-23001 CE") {
         window.location.href = "certificates/nnec.pdf";
       }
 
-      else if (input === "SHS/CE/PK-23002 ce") {
+      else if (input === "SHS/CE/PK-23002 CE") {
         window.location.href = "certificates/sivaahndlowy certificate CE-1.pdf";
       }
 
-           else if (input === "SHS/CE/PK-23003 ce") {
+           else if (input === "SHS/CE/PK-23003 CE") {
         window.location.href = "certificates/BMB certificate CE.pdf";
       }
           
