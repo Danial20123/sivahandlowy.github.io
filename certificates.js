77044be9c83196d755b259385326c3cf9d1d8971
Danial20123 <118391154+Danial20123@users.noreply.github.com>
2024-08-17 00:05:44 +0200

    Update certificates.js

--- certificates.js
+++ certificates.js
@@ -10,6 +10,15 @@ function redirectPage() {
         else if (input === "SHS/QMS/PK-24002 CE") {
       window.location.href = "certificates/Reeax CE.pdf";
     }
+          else if (input === "SHS/CE/PK-24001 CE") {
+      window.location.href = "certificates/Deft-Technik CE.pdf";
+    }
+                    else if (input === "SHS/QMS/PK-24001 13485") {
+      window.location.href = "certificates/Certificate 13485-1.pdf";
+    }
+                        else if (input === "SHS/QMS/PK-24001 9001") {
+      window.location.href = "certificates/Certificate 9001-1.pdf";
+    }
         
     else if (input === "SHS/QMS/PL/0001 13485") {
       window.location.href = "certificates/Document_2023-02-13_122728.pdf";
